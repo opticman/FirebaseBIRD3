d3155747d530a401da67929d3919c1eecb9a6622
Arsenal Pc <backhot99@homail.co.uk>
2017-11-25 15:02:11 +0700

    back controller succ

--- app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/RegisterFragment.java
+++ app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package com.nakarin.arsenalpc.firebasebird.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.nakarin.arsenalpc.firebasebird.MainActivity;
 import com.nakarin.arsenalpc.firebasebird.R;
 
 /**
@@ -15,6 +17,34 @@ import com.nakarin.arsenalpc.firebasebird.R;
 
 public class RegisterFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+    }//Main method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolberResgiter);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        setup title
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+
+//        back Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
 
     @Nullable
     @Override
