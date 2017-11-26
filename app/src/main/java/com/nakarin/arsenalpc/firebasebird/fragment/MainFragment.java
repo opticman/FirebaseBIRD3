c16db0d1727810e8f5b40812757d500505aeb7d5
Arsenal Pc <backhot99@homail.co.uk>
2017-11-26 11:19:47 +0700

    sing in Succse

--- app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/MainFragment.java
+++ app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/MainFragment.java
@@ -1,20 +1,34 @@
 package com.nakarin.arsenalpc.firebasebird.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nakarin.arsenalpc.firebasebird.R;
+import com.nakarin.arsenalpc.firebasebird.utility.MyAlertDialog;
+
 
 /**
- * Created by Arsenal Pc on 25-Nov-17.
+ * Created by masterung on 25/11/2017 AD.
  */
 
 public class MainFragment extends Fragment{
+
+    //    Explicit
+    private String emailString, passwordString;
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -23,20 +37,79 @@ public class MainFragment extends Fragment{
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
-    }//Main Method
+    }   // Main Method
+
+    private void loginController() {
+
+        Button button = getView().findViewById(R.id.btnlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Initial View
+                EditText emailEditText = getView().findViewById(R.id.edtemail);
+                EditText passwordEditText = getView().findViewById(R.id.edtpassword);
+
+//                Get Value From EditText
+                emailString = emailEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+//                Check Spec
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Spec
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.myNormalDialog("Have spec", getString(R.string.sub_register));
+                } else{
+//                    NO Space
+
+                    CheckEmailAnpass ();
+                }
+
+            }
+        });
+
+    }
+
+    private void CheckEmailAnpass() {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getActivity(),"welcome",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                            myAlertDialog.myNormalDialog("Authen False",
+                                    task.getException().getMessage());
+
+                        }
+                    }
+                });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewregister);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment())
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
                         .addToBackStack(null).commit();
 
-            }//onClick
-        });    }
+            }   // onClick
+        });
+    }
 
     @Nullable
     @Override
@@ -44,5 +117,4 @@ public class MainFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
-
-    } //main class
+}   // Main Class
--- app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/RegisterFragment.java
+++ app/src/main/java/com/nakarin/arsenalpc/firebasebird/fragment/RegisterFragment.java
@@ -1,6 +1,8 @@
 package com.nakarin.arsenalpc.firebasebird.fragment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -12,19 +14,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nakarin.arsenalpc.firebasebird.MainActivity;
 import com.nakarin.arsenalpc.firebasebird.R;
+import com.nakarin.arsenalpc.firebasebird.utility.MyAlertDialog;
+
 
 /**
- * Created by Arsenal Pc on 25-Nov-17.
+ * Created by masterung on 25/11/2017 AD.
  */
 
 public class RegisterFragment extends Fragment {
 
-    //    Explict
-    private String tag = "25Nov1";
+    //    Explicit
+    private String tag = "25NovV1";
     private String nameString, emailString, passwordString;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -33,39 +45,90 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
-//        create menu icon
-
+//        Create Menu Icon
         setHasOptionsMenu(true);
 
-    }//Main method
+
+    }   // Main Method
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (item.getItemId() == R.id.itemSave) {
-            CheckSpec();
+            checkSpace();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void CheckSpec() {
+    private void checkSpace() {
 
-        Log.d(tag, "CheckSpec Work");
+        Log.d(tag, "CheckSpace Work");
 
 //        Initial View
         EditText nameEditText = getView().findViewById(R.id.edtname);
         EditText emailEditText = getView().findViewById(R.id.edtemail);
-        EditText passwodEditText = getView().findViewById(R.id.edtpassword);
+        EditText passwordEditText = getView().findViewById(R.id.edtpassword);
 
-//        Get Value Frome EditText
+//        Get Value From EditText
         nameString = nameEditText.getText().toString().trim();
         emailString = emailEditText.getText().toString().trim();
-        passwordString = passwodEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+//        Check Space
+        if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+            MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+            myAlertDialog.myNormalDialog("Have Space",
+                    getString(R.string.sub_register));
+
+        } else {
+//            No Space
+            updateFirebase();
+
+        }
+
+    }   // checkSpace
+
+    private void updateFirebase() {
+
+//        Setup ProgressDialog
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Please Wait ...");
+        progressDialog.show();
 
 
-    }//Check spec
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        progressDialog.dismiss();
+
+                        if (task.isSuccessful()) {
+                            //Success
+                            Toast.makeText(getActivity(), "Update Firebase Success",
+                                    Toast.LENGTH_SHORT).show();
+                            getActivity().getSupportFragmentManager().popBackStack();
+
+                        } else {
+                            //Non Success
+                            MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                            myAlertDialog.myNormalDialog("Cannot Update Firebase",
+                                    task.getException().getMessage());
+
+                        }
+
+                    }
+                });
+
+
+
+
+    }   // updateFirebase
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -73,36 +136,48 @@ public class RegisterFragment extends Fragment {
         inflater.inflate(R.menu.menu_save, menu);
 
         super.onCreateOptionsMenu(menu, inflater);
-
     }
 
 
     private void createToolbar() {
+
+//        Config Toolbar
         Toolbar toolbar = getView().findViewById(R.id.toolberResgiter);
         ((MainActivity) getActivity()).setSupportActionBar(toolbar);
 
-//        setup title
-        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.register));
-        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+//        Setup Title and Sub
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setSubtitle(getString(R.string.sub_register));
 
-//        back Icon
-        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
-        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        Back Icon
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getActivity().getSupportFragmentManager().popBackStack();
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+
             }
         });
+
+
     }
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_register,
+                container, false);
 
         return view;
-
     }
-} //Main Class
+}   // Main Class
